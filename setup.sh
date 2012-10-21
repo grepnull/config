5487233f58138b64868bc437abf3bd4f3e05d4c5
vince rosso <v@grepnull.com>
2012-10-21 11:48:33 -0700

    added backup function to setup.sh
    will now backup currently existing bash files

--- setup.sh
+++ setup.sh
@@ -4,5 +4,16 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TO_LINK="bash_profile bashrc bashrc.d"
+BACKUP=~/.bashrc.backup.`date +%F_%H%M`
 
-for l in $TO_LINK; do echo "Linking $l"; rm -f ~/.$l; ln -sF $DIR/$l ~/.$l; done
+for l in $TO_LINK; do
+    if [ $(find ~/.$l -maxdepth 0 -not -type l) ]; then
+        echo "Backing up ~/.$l to $BACKUP"
+        mkdir -p $BACKUP
+        cp -R ~/.$l $BACKUP
+    fi
+    echo "Linking $l";
+    echo ""
+    rm -rf ~/.$l;
+    ln -sF $DIR/$l ~/.$l;
+done
