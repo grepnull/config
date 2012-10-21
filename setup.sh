f4cf1f4277de1ddc789e4e80ea9f1d31eb8d9f12
vince rosso <v@grepnull.com>
2012-10-21 11:54:30 -0700

    simplified file backup existence test

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ TO_LINK="bash_profile bashrc bashrc.d"
 BACKUP=~/.bashrc.backup.`date +%F_%H%M`
 
 for l in $TO_LINK; do
-    if [ $(find ~/.$l -maxdepth 0 -not -type l) ]; then
+    if [ -e ~/.$l ]; then
         echo "Backing up ~/.$l to $BACKUP"
         mkdir -p $BACKUP
         cp -R ~/.$l $BACKUP
