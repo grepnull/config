36b93f070a87b30de017a5a845175147c09fc7c6
vince rosso <v@grepnull.com>
2013-01-03 12:13:50 -0800

    sanity checks for emacs/vimpager and mac specific environment vars
    check if vimpager and emacs exist before setting PAGER and EDITOR
    check if on a mac before setting mac specific JAVA vars

--- bashrc.d/10-env.bash
+++ bashrc.d/10-env.bash
@@ -1,17 +1,27 @@
-export EDITOR='emacs -nw'
-export TZ='US/Pacific'
+if command -v emacs &> /dev/null; then
+    export EDITOR='emacs -nw'
+fi
+
+# export TZ='US/Pacific'
 # enable syntax highlighting for less
 export LESS=' -R '
-export PAGER=vimpager
-export FIGNORE=\~:CVS:.svn
 
-### Java ###
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/
-# Groovy on the mac defaults to MacRoman encoding
-export JAVA_OPTS=-Dfile.encoding=UTF-8
+if command -v vimpager &> /dev/null; then
+    export PAGER=vimpager
+fi
+
+export FIGNORE=\~:CVS:.svn
 
 # If we're on a Mac, get the mac's name as set in System Preferences->Sharing->Computer Name
 # can also use to detect if we're on a mac at all. If MAC_NAME is set then this is a mac
 if [ -e /usr/sbin/scutil ]; then
     export MAC_NAME=$(/usr/sbin/scutil --get ComputerName)
+fi
+
+if [ $MAC_NAME ]; then
+    ### Java on the Mac ###
+    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/
+    # Groovy on the mac defaults to MacRoman encoding
+    export JAVA_OPTS=-Dfile.encoding=UTF-8
+    export HOMEBREW_TEMP=/var/tmp
 fi
