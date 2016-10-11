2920bf1f21a05cb395d40f39ed9ba5d828eebf35
vince rosso <vince@zoox.com>
2016-10-10 17:09:00 -0700

    morea

--- emacs/personal/custom.el
+++ emacs/personal/custom.el
@@ -5,7 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-complete yaml-mode web-mode scss-mode yari inf-ruby ruby-tools company-anaconda anaconda-mode gotest go-projectile go-eldoc company-go go-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere ido-ubiquitous guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region elisp-slime-nav easy-kill dockerfile-mode discover-my-major diminish diff-hl crux company browse-kill-ring beacon anzu ace-window))))
+    (protobuf-mode markdown-mode auto-complete yaml-mode web-mode scss-mode yari inf-ruby ruby-tools company-anaconda anaconda-mode gotest go-projectile go-eldoc company-go go-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere ido-ubiquitous guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region elisp-slime-nav easy-kill dockerfile-mode discover-my-major diminish diff-hl crux company browse-kill-ring beacon anzu ace-window)))
+ '(safe-local-variable-values
+   (quote
+    ((sgml-indent-level . 2)
+     (nxml-attribute-indent . 3)
+     (nxml-child-indent . 3)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
